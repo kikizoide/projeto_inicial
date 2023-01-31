cf120c43cd57cf1cd1245d1e2a4c283bbdc2735b
luish <luishenrique.farina@gmail.com>
2023-01-31 16:03:55 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+main() {}
+
+// ignore: use_key_in_widget_constructors
+class PerguntaApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Text('Olá Flutter!'),
+    );
+  }
+}
